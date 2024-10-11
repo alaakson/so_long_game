f91da05ce3c88a6935c04e00a3e56b70375488d0
Anton Laaksonen <162610032+alaakson@users.noreply.github.com>
2024-10-11 11:22:36 +0300

    Coin checker added

--- sources/check_map_error.c
+++ sources/check_map_error.c
@@ -8,23 +8,56 @@ int check_tiles(t_game *game)
     int width;
 
     i = 1;
-    height = game->map.rows;
-    width = game->map.columns;
-    while (i < height - 1)
+    width = game->map.rows;
+    height = game->map.columns;
+    while (i < width - 1)
     {
         j = 1;
-        while (j < width - 1)
+        while (j < height - 1)
         {
-            char tile;
+            char    tile;
             tile = game->map.map[i][j];
-            if (tile != '0' && tile != '1' && tile != 'E' && tile != 'C'&& tile != 'P')
+            if (tile != '0' && tile != 'P' && tile != 'E' && tile != 'C' 
+                && tile != '1')
             {
-                ft_printf("Tiles missing (%d, %d) is not '0', found '%c'\n", i , j, tile);
+                ft_printf("Tiles missing here (%d, %d), found '%c'\n", i, j, tile);
                 exit(EXIT_FAILURE);
             }
-        j++;
+                j++;
         }
         i++;
+    }
+    return (1);
+}
+
+int check_coins(t_game *game)
+{
+    int j;
+    int i;
+    int height;
+    int width;
+    int coin_count = 0;
+
+    i = 0;
+    width = game->map.rows;
+    height = game->map.columns;
+    while (i < width)
+    {
+        j = 0;
+        while (j < height)
+        {
+            char    tile;
+            tile = game->map.map[i][j];
+            if (tile == 'C')
+                coin_count++;
+            j++;
+        }
+        i++;
+    } 
+    if (coin_count == 0)
+    {
+        ft_printf("\nERROR!\nNo coins found on the map\n");
+        exit(EXIT_FAILURE);
     }
     return (1);
 }
